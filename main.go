3697cb95483821aa6b0fe1e91facb80d01ed3b03
Jaroslav Viravec <jaroslav.viravec@sap.com>
2025-02-13 20:35:41 +0100

    SPDX-FileCopyrightText updated

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and valkey-operator-cop contributors
+SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and valkey-operator-cop contributors
 SPDX-License-Identifier: Apache-2.0
 */
 
